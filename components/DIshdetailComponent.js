c317c95c45f4ea52d7d47c55be4f714152be9010
Viktor Sivulskiy <svicvg@gmail.com>
2018-09-20 14:21:36 +0600

    A RenderDish Function Was Added

--- components/DIshdetailComponent.js
+++ components/DIshdetailComponent.js
@@ -2,3 +2,18 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { ListItem, Card } from 'react-native-elements';
 
+function RenderDish(props) {
+    const dish = props.dish;
+    if (dish !== null) {
+        return (<Card
+            featuredTitle={dish.name}
+            image={require('./images/uthappizza.png')}
+        >
+            <Text style={{ margin: 10 }}>
+                {dish.description}
+            </Text>
+        </Card>);
+    } else {
+        return (<View></View>);
+    }
+}
