e23e9d966f33101ad2c6a67478189c1816e244ee
Viktor Sivulskiy <svicvg@gmail.com>
2018-09-26 11:21:58 +0600

    A Function RenderItem Was Added

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,5 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { DISHES } from '../shared/dishes';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
+function RenderItem(props) {
+    
+    const item = props.item;
+    
+    if (item != null) {
+        return(
+            <Card
+                featuredTitle={item.name}
+                featuredSubtitle={item.designation}
+                image={require('./images/uthappizza.png')}>
+                <Text
+                    style={{margin: 10}}>
+                    {item.description}</Text>
+            </Card>
+        );
+    }
+    else {
+        return(<View></View>);
+    }
+}
 
 class Home extends Component {
 
