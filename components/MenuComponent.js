1f502585f199f4190708d3683c06b6b8c56623e1
Viktor Sivulskiy <svicvg@gmail.com>
2018-10-12 08:24:33 +0600

    The LoadingComponent.js Was Applied To The MenuComponent.js File

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -25,23 +25,36 @@ class Menu extends Component {
                     caption={item.description}
                     featured
                     onPress={() => navigate('Dishdetail', { dishId: item.id })}
-                    imageSrc={{ uri: baseUrl + item.image}}
+                    imageSrc={{ uri: baseUrl + item.image }}
                 />
             );
         };
 
         const { navigate } = this.props.navigation;
 
-        return (
-            <FlatList
-                data={this.props.dishes.dishes}
-                renderItem={renderMenuItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        );
+        if (this.props.dishes.isLoading) {
+            return (
+                <Loading />
+            );
+        }
+        else if (this.props.dishes.errMess) {
+            return (
+                <View>
+                    <Text>{this.props.dishes.errMess}</Text>
+                </View>
+            );
+        }
+        else {
+            return (
+                <FlatList
+                    data={this.props.dishes.dishes}
+                    renderItem={renderMenuItem}
+                    keyExtractor={item => item.id.toString()}
+                />
+            );
+        }
     }
 
 }
-
 
 export default connect(mapStateToProps)(Menu);
