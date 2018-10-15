538adb7559fcaf0cccc50a3c7c9d126275539f63
Viktor Sivulskiy <svicvg@gmail.com>
2018-10-15 08:11:33 +0600

    The MainComponent.js File Was Updated To Intergrate The Reservation Component

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import Home from './HomeComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Dishdetail from './DishdetailComponent';
+import Reservation from './ReservationComponent';
 import { View, Platform, Image, StyleSheet, ScrollView, Text } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
@@ -110,6 +111,23 @@ const ContactNavigator = createStackNavigator({
         })
     });
 
+const ReservationNavigator = createStackNavigator({
+    Reservation: { screen: Reservation }
+}, {
+        navigationOptions: ({ navigation }) => ({
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTitleStyle: {
+                color: "#fff"
+            },
+            headerTintColor: "#fff",
+            headerLeft: <Icon name="menu" size={24}
+                iconStyle={{ color: 'white' }}
+                onPress={() => navigation.navigate('DrawerToggle')} />
+        })
+    })
+
 const CustomDrawerContentComponent = (props) => (
     <ScrollView>
         <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
@@ -195,6 +213,22 @@ const MainNavigator = createDrawerNavigator({
             )
         },
     },
+    Reservation:
+    {
+        screen: ReservationNavigator,
+        navigationOptions: {
+            title: 'Reserve Table',
+            drawerLabel: 'Reserve Table',
+            drawerIcon: ({ tintColor }) => (
+                <Icon
+                    name='cutlery'
+                    type='font-awesome'
+                    size={24}
+                    iconStyle={{ color: tintColor }}
+                />
+            ),
+        }
+    }
 }, {
         drawerBackgroundColor: '#D1C4E9',
         contentComponent: CustomDrawerContentComponent
